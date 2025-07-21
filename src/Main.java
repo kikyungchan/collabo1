df76a645f90d51bd9c17daca7a07f5859b190cb6
kikyungchan <kikyungchan@gmail.com>
2025-07-21 09:31:05 +0900

    필요없는 파일 지우고 기타 설정 파일 작성

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("필요 없는 파일 지우기");
+        System.out.println("기타 필요한 설정 파일 작성하기");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
